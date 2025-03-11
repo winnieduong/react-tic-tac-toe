1f593e7f461be7a6f55dd052a05b06f041067674
winnieduong <khanhuyenhduong@gmail.com>
2025-03-11 18:26:53 -0400

    initiate square n css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Square() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="board-row">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
+    <div className="board-row">
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+    </div>
+    <div className="board-row">
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+    </div>
     </>
-  )
+  );
 }
-
-export default App
