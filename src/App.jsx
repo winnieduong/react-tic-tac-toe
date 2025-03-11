61f6e54cec7b11a9da2f2fd30ef003b078b2b587
winnieduong <khanhuyenhduong@gmail.com>
2025-03-11 18:41:17 -0400

    update handleclick function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+
 function Square({ value }) {
-  return <button className="square">{value}</button>;
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  ); 
 }
 
 export default function Board() {
