194441f3927e44ca3581a473ca6ba67f386130f3
winnieduong <khanhuyenhduong@gmail.com>
2025-03-11 18:32:55 -0400

    update table

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-export default function Square() {
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
   return (
     <>
-    <div className="board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-    </div>
-    <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-    </div>
-    <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
-    </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
