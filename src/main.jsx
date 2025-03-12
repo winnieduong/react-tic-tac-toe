46e44c58e2c2138611f0d24527148d189c935b6e
winnieduong <khanhuyenhduong@gmail.com>
2025-03-12 13:36:17 -0400

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Game from './App.jsx'
 
 const root = createRoot(document.getElementById("root"));
 root.render(
